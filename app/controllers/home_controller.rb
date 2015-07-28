e20445bd65b98b468d1e9e325a20d9d058cf3f14
torvik <pless.victor@gmail.com>
2015-07-28 20:57:52 -0300

    dashboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,9 +10,9 @@ class HomeController < ApplicationController
   end
 
   def dashboard
-    @products = Product.where(:user_id => current_user.id).last(5)
-    @sales = Sale.where(:user_id => current_user.id).last(5)
-    @customers = Customer.where(:user_id => current_user.id).last(5)
+    @products = Product.where(:user_id => current_user.id).order("created_at DESC").limit(5)
+    @sales = Sale.where(:user_id => current_user.id).order("created_at DESC").limit(5)
+    @customers = Customer.where(:user_id => current_user.id).order("created_at DESC").limit(5)
   end
 
 end
